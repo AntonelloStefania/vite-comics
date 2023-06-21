17f825a19cac06a26c47926f0a143b83072c975a
AntonelloStefania <antonelloste94@gmail.com>
2023-06-21 15:20:15 +0200

    [add] sezione main

---
+++ src/components/AppSerie.vue
@@ -0,0 +1,30 @@
+<script>
+    export default {
+       props:{
+         myCard : Object
+        }
+    }
+</script>
+<template>
+    <div class="container ">
+        <div >
+            <a href=""><img :src="myCard.thumb" alt="" class="py-3"></a>
+            <h6 class="pt-2 text-center pb-3">{{ myCard.series.toUpperCase() }}</h6>
+        </div>
+    </div>
+</template>
+<style lang="scss" scoped>
+    img{
+        height: 300px;
+        width:300px;
+    }
+
+    img:hover{
+        opacity: 0.5;
+    }
+
+    h6{
+        color:rgb(255, 255, 255);
+        font-size: 12px;
+    }
+</style>
